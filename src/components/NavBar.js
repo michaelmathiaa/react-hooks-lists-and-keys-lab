9f83cfeb7636f7498f0f44c945e5111dc8a0261c
Michael Mathias <michaelscodingjourney@gmail.com>
2023-05-28 18:35:53 -0700

    Added navBar, projectList, and projectItem content with map

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const link = links.map(link => <a key = {link} href = {'#' + link}>{link}</a>);
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{link}</nav>;
 }
 
 export default NavBar;
